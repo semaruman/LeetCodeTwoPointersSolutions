f461950abe285c7b403bf7a8cf06e7679d682ce1
Semen <semaruman@yandex.ru>
2026-05-17 12:30:58 +0300

    тестовый коммит 2

--- LeetCodeTwoPointersSolutions/Easy/ValidPalindrome.cs
+++ LeetCodeTwoPointersSolutions/Easy/ValidPalindrome.cs
@@ -35,7 +35,6 @@ namespace LeetCodeTwoPointersSolutions.Easy.ValidPalindrome
             return true;
         }
 
-
         //простое решение без оптимизации (18 ms | Beats 54.91%)
         /*
         public bool IsPalindrome(string s)
