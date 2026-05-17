8b47ada5a213d20229d077c59ec3f9eb1e20dc82
Semen <semaruman@yandex.ru>
2026-05-17 22:09:44 +0300

    Решил задачу Squares of a Sorted Array уровня Easy 1 ms | Beats 100.00%

---
+++ LeetCodeTwoPointersSolutions/Easy/SquaresOfaSortedArray.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeTwoPointersSolutions.Easy
+{
+    public class Solution
+    {
+        public int[] SortedSquares(int[] nums)
+        {
+            int[] sortedSquares = new int[nums.Length];
+            int pointer = nums.Length - 1;
+
+            int left = 0;
+            int right = nums.Length - 1;
+
+            while (left <= right)
+            {
+                if (nums[left] * nums[left] < nums[right] * nums[right])
+                {
+                    sortedSquares[pointer] = nums[right] * nums[right];
+                    right--;
+                }
+                else
+                {
+                    sortedSquares[pointer] = nums[left] * nums[left];
+                    left++;
+                }
+                pointer--;
+            }
+
+            return sortedSquares;
+        }
+    }
+}
