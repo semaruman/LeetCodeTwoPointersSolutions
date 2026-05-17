d6c5c6b9ff3deba6621cb6c9d7936d9fd023756e
Semen <semaruman@yandex.ru>
2026-05-17 11:42:49 +0300

    Решил задачу Valid Palindrome уровня Easy 11 ms | Beats 88.87%

---
+++ LeetCodeTwoPointersSolutions/Easy/ValidPalindrome.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeTwoPointersSolutions.Easy.ValidPalindrome
+{
+    public class Solution
+    {
+        public bool IsPalindrome(string s)
+        {
+            int left = 0;
+            int right = s.Length - 1;
+
+            while (left < right)
+            {
+                while (left < right && !char.IsLetterOrDigit(s[left]))
+                {
+                    left++;
+                }
+
+                while (left < right && !char.IsLetterOrDigit(s[right]))
+                {
+                    right--;
+                }
+
+                if (char.ToLower(s[left]) != char.ToLower(s[right]))
+                {
+                    return false;
+                }
+
+                left++;
+                right--;
+            }
+
+            return true;
+        }
+
+        //простое решение без оптимизации (18 ms | Beats 54.91%)
+        /*
+        public bool IsPalindrome(string s)
+        {
+            string str = new string(s.Where(c => char.IsLetterOrDigit(c)).ToArray()).ToLower();
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i] != str[str.Length - i - 1])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+        */
+    }
+}
