542e0e0c6b8308611e0ded1520f0b543624f6710
Semen <semaruman@yandex.ru>
2026-05-17 12:56:42 +0300

    решил задачу MoveZeroes уровня Easy 1 ms | Beats 96.97%

---
+++ LeetCodeTwoPointersSolutions/Easy/MoveZeroes.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeTwoPointersSolutions.Easy.MoveZeroes
+{
+    public class Solution
+    {
+        public void MoveZeroes(int[] nums)
+        {
+            int lastNonZeroPointer = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] != 0)
+                {
+                    nums[lastNonZeroPointer] = nums[i];
+                    lastNonZeroPointer++;
+                }
+            }
+
+            for (int i = lastNonZeroPointer; i < nums.Length; i++)
+            {
+                nums[i] = 0;
+            }
+        }
+    }
+}
