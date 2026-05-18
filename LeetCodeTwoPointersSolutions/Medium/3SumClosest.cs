d086e1ccb00abec6cd1815ab5ed53264a95f753b
Semen <semaruman@yandex.ru>
2026-05-18 06:39:25 +0300

    Решил задачу 3Sum Closest уровня medium 13 ms | Beats 87.82%

---
+++ LeetCodeTwoPointersSolutions/Medium/3SumClosest.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeTwoPointersSolutions.Medium.ThreeSumClosest
+{
+    public class Solution
+    {
+        public int ThreeSumClosest(int[] nums, int target)
+        {
+            Array.Sort(nums);
+            int left;
+            int rigth;
+            int resSum = nums[0] + nums[1] + nums[2];
+
+            for (int i = 0; i < nums.Length - 2; i++)
+            {
+                left = i + 1;
+                rigth = nums.Length - 1;
+
+                while (left < rigth)
+                {
+                    int sum = nums[i] + nums[left] + nums[rigth];
+                    if (sum == target)
+                        return target;
+
+                    if (Math.Abs(sum - target) < Math.Abs(resSum - target))
+                    {
+                        resSum = sum;
+                    }
+
+                    if (sum < target)
+                    {
+                        left++;
+                    }
+                    else if (sum > target)
+                    {
+                        rigth--;
+                    }
+
+                }
+            }
+
+            return resSum;
+        }
+    }
+}
