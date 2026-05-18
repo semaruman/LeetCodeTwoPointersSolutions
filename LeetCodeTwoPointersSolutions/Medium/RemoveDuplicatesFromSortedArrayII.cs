f60c107aae0a58cd5bbd0eed345495be292f65e1
Semen <semaruman@yandex.ru>
2026-05-18 06:17:15 +0300

    Решил задачу Remove Duplicates from Sorted Array II уровня medium 130 ms | Beats 87.69%

---
+++ LeetCodeTwoPointersSolutions/Medium/RemoveDuplicatesFromSortedArrayII.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeTwoPointersSolutions.Medium.RemoveDuplicates
+{
+    public class Solution
+    {
+        public int RemoveDuplicates(int[] nums)
+        {
+            if (nums.Length < 2)
+            {
+                return nums.Length;
+            }
+
+            int currentPointer = 2;
+            for (int i = 2; i < nums.Length; i++)
+            {
+                if (nums[currentPointer - 2] != nums[i])
+                {
+                    nums[currentPointer] = nums[i];
+                    currentPointer++;
+                }
+            }
+
+            return currentPointer;
+        }
+    }
+}
--- LeetCodeTwoPointersSolutions/Medium/Two Sum2InputArrayIsSorted.cs
+++ LeetCodeTwoPointersSolutions/Medium/Two Sum2InputArrayIsSorted.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace LeetCodeTwoPointersSolutions.Medium
+namespace LeetCodeTwoPointersSolutions.Medium.TwoSum2InputArrayIsSorted
 {
     public class Solution
     {
