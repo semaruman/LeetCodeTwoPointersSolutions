6278fc28e3e8003f64de3aea7052dabd074ceca9
Semen <semaruman@yandex.ru>
2026-05-18 06:53:52 +0300

    Решил задачу Assign Cookies уровня Easy 47 ms | Beats 96.69%

---
+++ LeetCodeTwoPointersSolutions/Easy/AssignCookies.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeTwoPointersSolutions.Easy.AssignCookies
+{
+    public class Solution
+    {
+        public int FindContentChildren(int[] g, int[] s)
+        {
+            Array.Sort(g);
+            Array.Sort(s);
+
+            int cookiePointer = 0;
+            int childPointer = 0;
+
+            while (cookiePointer < s.Length && childPointer < g.Length)
+            {
+                if (s[cookiePointer] >= g[childPointer])
+                {
+                    childPointer++;
+                }
+                cookiePointer++;
+            }
+
+            return childPointer;
+        }
+    }
+}
