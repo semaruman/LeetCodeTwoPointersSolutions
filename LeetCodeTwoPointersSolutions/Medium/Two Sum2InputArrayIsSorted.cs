490369d1da6335d7cf1781c1b763fb1b56af0fe9
Semen <semaruman@yandex.ru>
2026-05-18 05:53:07 +0300

    Решил задачу Two Sum II - Input Array Is Sorted уровня medium 0 ms | Beats 100.00%

---
+++ LeetCodeTwoPointersSolutions/Medium/Two Sum2InputArrayIsSorted.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeTwoPointersSolutions.Medium
+{
+    public class Solution
+    {
+        public int[] TwoSum(int[] numbers, int target)
+        {
+            int left = 0;
+            int right = numbers.Length - 1;
+
+            int sum = 0;
+
+            while (left < right)
+            {
+                sum = numbers[left] + numbers[right];
+                if (sum > target)
+                {
+                    right--;
+                }
+                else if ( sum < target)
+                {
+                    left++;
+                }
+                else
+                {
+                    return new int[] {  left + 1, right + 1 };
+                }
+            }
+
+            return null;
+        }
+    }
+}
