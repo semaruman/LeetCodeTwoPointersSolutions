38a63b01b74a4c0d9756a8ccf7d3cd4e9694fc8e
Semen <semaruman@yandex.ru>
2026-05-19 08:19:16 +0300

    решил задачу Sort Colors уровня Medium 0 ms | Beats 100.00%

---
+++ LeetCodeTwoPointersSolutions/Medium/SortColors.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeTwoPointersSolutions.Medium.SortColors
+{
+    public class Solution
+    {
+        public void SortColors(int[] nums)
+        {
+            int redPointer = 0;
+            int bluePointer = nums.Length - 1;
+            int currentPointer = 0;
+
+            while (currentPointer <= bluePointer)
+            {
+                if (nums[currentPointer] == 0)
+                {
+                    (nums[currentPointer], nums[redPointer]) = (nums[redPointer], nums[currentPointer]);
+                    redPointer++;
+                    currentPointer++;
+                }
+                else if (nums[currentPointer] == 1)
+                {
+                    currentPointer++;
+                }
+                else
+                {
+                    (nums[currentPointer], nums[bluePointer]) = (nums[bluePointer], nums[currentPointer]);
+                    bluePointer--;
+                }
+            }
+        }
+    }
+}
