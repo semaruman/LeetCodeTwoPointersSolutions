3c98b60892f0e73764000f9b6376fa26b25958f4
Semen <semaruman@yandex.ru>
2026-05-19 08:46:48 +0300

    Решил задачу Minimum Length of String After Deleting Similar Ends уровня medium 1 ms | Beats 100.00%

---
+++ LeetCodeTwoPointersSolutions/Medium/MinimumLengthOfStringAfterDeletingSimilarEnds.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeTwoPointersSolutions.Medium.MinimumLengthOfStringAfterDeletingSimilarEnds
+{
+    public class Solution
+    {
+        public int MinimumLength(string s)
+        {
+            int left = 0;
+            int right = s.Length - 1;
+
+            while (left < right && s[left] == s[right])
+            {
+                char c = s[left];
+
+                while (left <= right && s[left] == c)
+                {
+                    left++;
+                }
+                while (left <= right && s[right] == c)
+                {
+                    right--;
+                }
+            }
+
+            return right - left + 1;
+        }
+    }
+}
